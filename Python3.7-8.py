07722bcd0d69432308c36eee7b113619640b6a54
BorsosDominik <90307311+BorsosDominik@users.noreply.github.com>
2021-11-30 08:53:35 +0100

    Add files via upload

---
+++ Python3.7-8.py
@@ -0,0 +1,30 @@
+#python 3.7-8.feladat
+import turtle
+
+turtle.setheading(0)
+turtle.speed(1)
+turtle.left(160)
+turtle.forward(100)
+turtle.right(-43)
+turtle.forward(100)
+turtle.left(270)
+turtle.forward(100)
+turtle.right(-97)
+turtle.forward(100)
+turtle.right(-43)
+turtle.forward(100)
+turtle.left(200)
+turtle.forward(100)
+turtle.right(-940)
+turtle.forward(100)
+turtle.left(17)
+turtle.forward(100) 
+turtle.right(-86)
+
+print(turtle.pos())
+
+a = turtle.Screen()
+
+a.title("Python3-7")
+a.mainloop()
+
